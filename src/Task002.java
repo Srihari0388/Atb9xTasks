f998b59b6fa53b2a1026d0eddbafcf62b5f0e48b
Srihari Babu Kolla <sriharibabu.kolla@gmail.com>
2024-10-10 18:41:56 +0530

    Atb Tasks Daily

---
+++ src/Task002.java
@@ -0,0 +1,15 @@
+
+public class Task002 {
+
+	public static void main(String[] args) {
+		System.out.println("10*1=10");
+		System.out.println("10*2=20");
+		System.out.println("10*3=30");
+		System.out.println("10*4=40");
+		System.out.println("10*5=50");
+		System.out.println("----");
+		System.out.println("10*10=100");
+		
+	}
+
+}
